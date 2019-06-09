05a7f93602f0bcdef993df19ad81fa48d6eb3be8
Luís Miguel Ribeiro Moreira <lmr_moreira@live.com.pt>
2019-06-09 23:06:04 +0100

    Final Antes da entrega. Falta explosão e robot

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -201,42 +201,42 @@ function render()
 /////////////////////////////////
 function ExplodeAnimation(x,y)
 {
-  var geometry = new THREE.Geometry();
-  
-  for (let i = 0; i < totalObjects; i ++) 
-  { 
-    var vertex = new THREE.Vector3();
-    vertex.x = x;
-    vertex.y = y;
-    vertex.z = 0;
-  
-    geometry.vertices.push( vertex );
-    dirs.push({x:(Math.random() * movementSpeed)-(movementSpeed/2),y:(Math.random() * movementSpeed)-(movementSpeed/2),z:(Math.random() * movementSpeed)-(movementSpeed/2)});
-  }
-  var material = new THREE.ParticleBasicMaterial( { size: objectSize,  color: colors[Math.round(Math.random() * colors.length)] });
-  var particles = new THREE.ParticleSystem( geometry, material );
-  
-  this.object = particles;
-  this.status = true;
-  
-  this.xDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-  this.yDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-  this.zDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-  
-  scene.add( this.object  ); 
-  
-  this.update = function(){
-    if (this.status == true){
-      var pCount = totalObjects;
-      while(pCount--) {
-        var particle =  this.object.geometry.vertices[pCount]
-        particle.y += dirs[pCount].y;
-        particle.x += dirs[pCount].x;
-        particle.z += dirs[pCount].z;
-      }
-      this.object.geometry.verticesNeedUpdate = true;
+    var geometry = new THREE.Geometry();
+
+    for (let i = 0; i < totalObjects; i ++) 
+    { 
+        var vertex = new THREE.Vector3();
+        vertex.x = x;
+        vertex.y = y;
+        vertex.z = 0;
+
+        geometry.vertices.push( vertex );
+        dirs.push({x:(Math.random() * movementSpeed)-(movementSpeed/2),y:(Math.random() * movementSpeed)-(movementSpeed/2),z:(Math.random() * movementSpeed)-(movementSpeed/2)});
     }
-  }
+    var material = new THREE.ParticleBasicMaterial( { size: objectSize,  color: colors[Math.round(Math.random() * colors.length)] });
+    var particles = new THREE.ParticleSystem( geometry, material );
+
+    this.object = particles;
+    this.status = true;
+
+    this.xDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+    this.yDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+    this.zDir = (Math.random() * movementSpeed)-(movementSpeed/2);
+
+    scene.add( this.object  ); 
+
+    this.update = function(){
+        if (this.status == true){
+            var pCount = totalObjects;
+            while(pCount--) {
+                var particle =  this.object.geometry.vertices[pCount]
+                particle.y += dirs[pCount].y;
+                particle.x += dirs[pCount].x;
+                particle.z += dirs[pCount].z;
+            }
+            this.object.geometry.verticesNeedUpdate = true;
+        }
+    }
   
 }
 /////////////////////////////////
@@ -307,7 +307,7 @@ function PerderVida()
         EscreverTexto("Perdeu"); 
     } else 
     {
-        IniciarJogoMenosVida();
+        setTimeout(IniciarJogoMenosVida(), 3000);
     }
 }
 
@@ -533,25 +533,25 @@ function CarregarTexturas()
     var texture = new THREE.TextureLoader().load( "../Texturas/bathroom.jpg" );
     texture.wrapS = THREE.MirroredRepeatWrapping;
     texture.wrapT = THREE.MirroredRepeatWrapping;
-    texture.repeat.set( 4, 4 );
+    texture.repeat.set( 2, 1 );
     Texturas.push(texture);
 
     texture = new THREE.TextureLoader().load( "../Texturas/brick-wall.jpg" );
     texture.wrapS = THREE.MirroredRepeatWrapping;
     texture.wrapT = THREE.MirroredRepeatWrapping;
-    texture.repeat.set( 4, 4 );
+    texture.repeat.set( 1, 1 );
     Texturas.push(texture);
 
-    texture = new THREE.TextureLoader().load( "../Texturas/roughness-map.jpg" );
+    texture = new THREE.TextureLoader().load( "../Texturas/1.jpg" );
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 4, 4 );
+    texture.repeat.set( 1, 1 );
     Texturas.push(texture);
 
     texture = new THREE.TextureLoader().load( "../Texturas/stone.jpg" );
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set( 4, 4 );
+    texture.repeat.set( 1, 1 );
     Texturas.push(texture);
 
     texture = new THREE.TextureLoader().load( "../Texturas/wood.jpg" );
@@ -570,6 +570,12 @@ function CarregarTexturas()
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set( 1, 1 );
+    Texturas.push(texture);
+
+    texture = new THREE.TextureLoader().load( "../Texturas/Plane.jpg" );
+    texture.wrapS = THREE.RepeatWrapping;
+    texture.wrapT = THREE.RepeatWrapping;
+    texture.repeat.set( 1, 2 );
     Texturas.push(texture);
 
 }
@@ -675,7 +681,7 @@ function createSideWalls()
     scene.add(upWall);
 }
 
-
+//plano de fundo
 function createBackWall()
 {
     const geometry = new THREE.PlaneGeometry( PLANEWIDTH, PLANEHEIGHT );
@@ -779,102 +785,4 @@ window.onload = init;
 
 
 
-
-
-
-
-
-// //////////////settings/////////
-// var movementSpeed = 80;
-// var totalObjects = 1000;
-// var objectSize = 10;
-// var sizeRandomness = 4000;
-// var colors = [0xFF0FFF, 0xCCFF00, 0xFF000F, 0x996600, 0xFFFFFF];
-// /////////////////////////////////
-// var dirs = [];
-// var parts = [];
-// var container = document.createElement('div');
-// document.body.appendChild( container );
-
-// var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight,1, 10000)
-// camera.position.z = 1000; 
-
-// var scene = new THREE.Scene(); 
-
-// function ExplodeAnimation(x,y)
-// {
-//   var geometry = new THREE.Geometry();
-  
-//   for (i = 0; i < totalObjects; i ++) 
-//   { 
-//     var vertex = new THREE.Vector3();
-//     vertex.x = x;
-//     vertex.y = y;
-//     vertex.z = 0;
-  
-//     geometry.vertices.push( vertex );
-//     dirs.push({x:(Math.random() * movementSpeed)-(movementSpeed/2),y:(Math.random() * movementSpeed)-(movementSpeed/2),z:(Math.random() * movementSpeed)-(movementSpeed/2)});
-//   }
-//   var material = new THREE.ParticleBasicMaterial( { size: objectSize,  color: colors[Math.round(Math.random() * colors.length)] });
-//   var particles = new THREE.ParticleSystem( geometry, material );
-  
-//   this.object = particles;
-//   this.status = true;
-  
-//   this.xDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-//   this.yDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-//   this.zDir = (Math.random() * movementSpeed)-(movementSpeed/2);
-  
-//   scene.add( this.object  ); 
-  
-//   this.update = function(){
-//     if (this.status == true){
-//       var pCount = totalObjects;
-//       while(pCount--) {
-//         var particle =  this.object.geometry.vertices[pCount]
-//         particle.y += dirs[pCount].y;
-//         particle.x += dirs[pCount].x;
-//         particle.z += dirs[pCount].z;
-//       }
-//       this.object.geometry.verticesNeedUpdate = true;
-//     }
-//   }
-  
-// }
-
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// container.appendChild( renderer.domElement );
-
-// renderer.render( scene, camera );
-// parts.push(new ExplodeAnimation(0, 0));
-// render();
-
-// 			function render() {
-//         requestAnimationFrame( render );
-         
-//         var pCount = parts.length;
-//           while(pCount--) {
-//             parts[pCount].update();
-//           }
-
-// 				renderer.render( scene, camera );
-
-// 			}
-
-// window.addEventListener( 'mousedown', onclick, false );
-// window.addEventListener( 'resize', onWindowResize, false );
-
-// function onclick(){
-//   event.preventDefault();
-//   parts.push(new ExplodeAnimation((Math.random() * sizeRandomness)-(sizeRandomness/2), (Math.random() * sizeRandomness)-(sizeRandomness/2)));
-// }
-
-// function onWindowResize() {
-// 				camera.aspect = window.innerWidth / window.innerHeight;
-// 				camera.updateProjectionMatrix();
-
-// 				renderer.setSize( window.innerWidth, window.innerHeight );
-
-// 			}
 
